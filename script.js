e0e083aa9c551487fb40e5b04042542d05ba30a2
Torfa <alextison@icloud.com>
2022-03-18 15:32:28 +0100

    Remote Work

---
+++ script.js
@@ -0,0 +1,56 @@
+containerArrows = document.getElementsByClassName("arrows");
+containerLogo = document.getElementsByClassName("logo");
+containerPoster = document.getElementsByClassName("poster");
+containerBar = document.getElementsByClassName("bar");
+
+function previous(){
+    which = 'previous';
+    i = parseInt(containerArrows[0].getAttribute('data-position'));
+    if( i != 0){
+        logo();
+        poster();
+        bar();
+        containerArrows[0].setAttribute('data-position', i-1);
+    }
+}
+
+function next(){
+    which = 'next';
+    i = parseInt(containerArrows[0].getAttribute('data-position'));
+    if(i != 2){
+        logo();
+        poster();
+        bar();
+        containerArrows[0].setAttribute('data-position', i+1);
+    }
+}
+
+function logo(){
+    if(which == 'previous'){
+        containerLogo[0]['children'][i].style.display = 'none';
+        containerLogo[0]['children'][i-1].style.display = 'flex';
+    }else{
+        containerLogo[0]['children'][i].style.display = 'none';
+        containerLogo[0]['children'][i+1].style.display = 'flex';
+    }
+}
+
+function poster(){
+    if(which == 'previous'){
+        containerPoster[0]['children'][i].style.display = 'none';
+        containerPoster[0]['children'][i-1].style.display = 'flex';
+    }else{
+        containerPoster[0]['children'][i].style.display = 'none';
+        containerPoster[0]['children'][i+1].style.display = 'flex';
+    }
+}
+
+function bar(){
+    if(which == 'previous'){
+        containerBar[0]['children'][i-1].style.backgroundPosition = 'right';
+        containerBar[0]['children'][i]['children'][0].style.backgroundColor = '#827B68';
+    }else{
+        containerBar[0]['children'][i].style.backgroundPosition = 'left';
+        containerBar[0]['children'][i+1]['children'][0].style.backgroundColor = '#FFF38A';
+    }
+}
